16bfea4825d4fed0ecc79c1248acfbfed20f61a1
parunphon <46772010+parunphon@users.noreply.github.com>
2022-08-30 16:31:26 +0700

    clear2

--- shipment/file/DW_Duplicated_Shipment.sql
+++
@@ -1,7 +0,0 @@
-SELECT PreDONo, COUNT(*)
-  FROM ShipmentTracking
-  where 1=1
-  and OrderType in ('SO-ECO')
-  and PERSPECTIVE in ('B')
-  group by PreDONo
-  having COUNT(*) > 1
--- shipment/file/DW_check_SellShipment_missing.sql
+++
@@ -1,7 +0,0 @@
-Select [PreDONo]
-FROM [dbo].[ShipmentTracking]
-where [SALES_ORGANIZATION] in ('1002')
-      and [Logistics Confirm POD Date] is not null
-      and [Logistics Confirm POD Date] >= dbo.getlocaldate()-360
-group by [PreDONo]
-having count(*) = 1
